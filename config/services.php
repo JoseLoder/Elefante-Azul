6e57178745bfdeaca3b02d02234906af20f61d1a
MartinSilvaJose <118887247+MartinSilvaJose@users.noreply.github.com>
2024-05-11 21:35:55 +0200

    Delete duplicate redirect in service.php

--- config/services.php
+++ config/services.php
@@ -33,7 +33,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/callback',
         'redirect' => env('GOOGLE_REDIRECT_URL'),
     ]
 
